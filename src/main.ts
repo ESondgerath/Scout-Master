e37b12b9ccf12618fa2df550b2862ecd47ede677
Evan Sondgerath <esondgerath@gmail.com>
2018-07-23 11:36:49 -0400

    material maybe

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { 
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatButtonToggleModule,
+  MatDialogModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatMenuModule,
+  MatPaginatorModule,
+  MatRadioModule,
+  MatSidenavModule,
+  MatSlideToggleModule,
+  MatTableModule,
+  MatToolbarModule,
+  MatTooltipModule, 
+  MatSnackBarModule,} from "@angular/material";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
