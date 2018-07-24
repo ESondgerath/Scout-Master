07b57e59260d3f2712164afcd82756863df6a200
Evan Sondgerath <esondgerath@gmail.com>
2018-07-24 11:48:00 -0400

    NavBar Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { FooterComponent } from './ui/footer/footer.component';
 import { LayoutComponent } from './ui/layout/layout.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule } from '@angular/material';
 import { CdkTableModule } from '@angular/cdk/table';
 
 @NgModule({
@@ -27,6 +27,7 @@ import { CdkTableModule } from '@angular/cdk/table';
     MatButtonModule,
     BrowserAnimationsModule,
     CdkTableModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
