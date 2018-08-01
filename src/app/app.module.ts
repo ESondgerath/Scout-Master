d7c16c4515ea2b13eb4eb627ebbcafde9b1d62b7
Evan Sondgerath <esondgerath@gmail.com>
2018-08-01 11:23:24 -0400

    Profile Comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,6 +53,7 @@ import { NewPlayerModelComponent } from './search/new-player-model/new-player-mo
 import { AuthGuard } from './guard/auth.guard';
 import { AlertsComponent } from './alerts/alerts.component';
 import { EditPlayerModalComponent } from './search/edit-player-modal/edit-player-modal.component';
+import { EditprofileComponent } from './profile/editprofile/editprofile.component';
 
 
 @NgModule({
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,42 +2,55 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { UserService } from '../services/user.service';
 import { User } from '../models/user';
+import { MatDialog } from '@angular/material';
+import { Router } from '@angular/router';
+import { EditprofileComponent } from './editprofile/editprofile.component';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
-  providers: [ UserService ]
+  // providers: [ UserService ]
 })
 
 export class ProfileComponent implements OnInit {
 
-  editProfileForm: FormGroup;
-  // user: User
+  user: User[];
 
   constructor(
-    public userService: UserService,
-    private formBuilder: FormBuilder,
-    @Inject(User) public data: { response: User }
+    private userService: UserService,
+    private dialog: MatDialog
+    // @Inject(DATA) private data: { response: User }
     // public data: { response: User }
     // @Inject(User) public data
   ) { }
 
   ngOnInit() {
-    console.log(this.data);
-    console.log(this.data.response);
-    this.editProfileForm = this.formBuilder.group({
-      id: [this.data.response.id],
-      username: [this.data.response.username],
-      email: [this.data.response.email],
-      password: [this.data.response.password]
-    });
-    console.log(this.userService.user);
-  }
-  
-  onSubmit() {
-    this.userService.updateProfile(this.editProfileForm.value)
-    .subscribe()
+    // console.log(this.data);
+    // console.log(this.data.response);
+    // this.editProfileForm = this.formBuilder.group({
+    //   id: [this.data.response.id],
+    //   username: [this.data.response.username],
+    //   email: [this.data.response.email],
+    //   password: [this.data.response.password]
+    // });
+    // console.log(this.userService.user);
   }
 
+  // onSubmit() {
+  //   this.userService.updateProfile(this.editProfileForm.value)
+  //   .subscribe()
+  // }
+
+  openEditDialog(userId): void {
+    this.userService.getUserById(userId).subscribe(response => {
+      let dialogRef = this.dialog.open(EditprofileComponent, {
+        height: '23em',
+        width: '25em',
+        data: {response}
+      });
+      console.log(response)
+    })
+    console.log(userId);
+  }
 }
